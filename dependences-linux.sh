058a4a2b4e74e9b9ba61f78629b31a2e1c717b4f
Mendax <kirarisenpai@protonmail.com>
2021-01-28 01:40:36 -0300

    Add files via upload

---
+++ dependences-linux.sh
@@ -0,0 +1,16 @@
+
+apt-get install gcc cmake openssl libssl-dev libsqlite3-dev git
+git clone https://github.com/jedisct1/libsodium --branch stable
+cd libsodium
+./configure
+make && make check
+make install
+cd ..
+git clone https://github.com/P-H-C/phc-winner-argon2.git
+cd phc-winner-argon2
+make && make install PREFIX=/usr
+cd ..
+mkdir build && cd build
+export CC=`which gcc`
+cmake ..
+make install
